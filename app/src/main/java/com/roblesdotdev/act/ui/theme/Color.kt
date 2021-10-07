911a3361fffdf59edd91a47131e0866910ab458a
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2021-10-07 12:33:38 -0300

    Adding light color palette (#12)

--- app/src/main/java/com/roblesdotdev/act/ui/theme/Color.kt
+++ app/src/main/java/com/roblesdotdev/act/ui/theme/Color.kt
@@ -1,9 +1,21 @@
 @file:Suppress("MagicNumber")
 package com.roblesdotdev.act.ui.theme
 
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+private val PrimaryBlue = Color(0xFF111C27)
+private val SecondaryOrange = Color(0xFFF49D37)
+private val LightGray = Color(0xFFF4F4F8)
+private val DarkGray = Color(0xFF282828)
+
+val lightColorPalette = lightColors(
+    primary = PrimaryBlue,
+    onPrimary = Color.White,
+    secondary = SecondaryOrange,
+    onSecondary = Color.White,
+    background = LightGray,
+    onBackground = DarkGray,
+    surface = Color.White,
+    onSurface = PrimaryBlue
+)
