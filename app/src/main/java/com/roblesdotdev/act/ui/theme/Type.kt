3126372b24c1b8c82cce4e37a5b6af0b9ce08e6c
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2021-10-07 16:59:05 -0300

    Fixes #6 - Adding Typography (#13)

--- app/src/main/java/com/roblesdotdev/act/ui/theme/Type.kt
+++ app/src/main/java/com/roblesdotdev/act/ui/theme/Type.kt
@@ -2,27 +2,45 @@ package com.roblesdotdev.act.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.roblesdotdev.act.R
+
+private val HeeboExtraBold = FontFamily(Font(R.font.heebo_extrabold))
+private val HeeboBold = FontFamily(Font(R.font.heebo_bold))
+private val HeeboLight = FontFamily(Font(R.font.heebo_light))
+private val HeeboMedium = FontFamily(Font(R.font.heebo_medium))
+private val HeeboRegular = FontFamily(Font(R.font.heebo_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    h1 = TextStyle(
+        fontFamily = HeeboExtraBold,
+        fontSize = 40.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = HeeboExtraBold,
+        fontSize = 36.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = HeeboMedium,
+        fontSize = 14.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = HeeboMedium,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    ),
+    body1 = TextStyle(
+        fontFamily = HeeboLight,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = HeeboRegular,
         fontSize = 12.sp
+    ),
+    button = TextStyle(
+        fontFamily = HeeboBold,
+        fontSize = 14.sp
     )
-    */
 )
