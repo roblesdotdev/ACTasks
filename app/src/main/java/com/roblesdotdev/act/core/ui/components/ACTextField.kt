01c202c8a969db3f96f77cb0c81b0afb488696a7
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2021-10-13 10:20:35 -0300

    Transform LoginViewState to sealed class and update UI (#29)

--- app/src/main/java/com/roblesdotdev/act/core/ui/components/ACTextField.kt
+++ app/src/main/java/com/roblesdotdev/act/core/ui/components/ACTextField.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.roblesdotdev.act.R
@@ -21,16 +24,35 @@ fun ACTextField(
     text: String,
     onTextChanged: (String) -> Unit,
     labelText: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    errorMessage: String? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
 ) {
-    OutlinedTextField(
-        value = text, onValueChange = onTextChanged, label = {
-            Text(text = labelText)
-        },
-        modifier = modifier
-            .heightIn(dimensionResource(id = R.dimen.text_field_height))
-            .fillMaxWidth()
-    )
+    Column {
+        OutlinedTextField(
+            value = text, onValueChange = onTextChanged,
+            label = {
+                Text(text = labelText)
+            },
+            modifier = modifier
+                .heightIn(dimensionResource(id = R.dimen.text_field_height))
+                .fillMaxWidth(),
+            isError = (errorMessage != null),
+            visualTransformation = visualTransformation,
+        )
+
+        if (errorMessage != null) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colors.error,
+                modifier = Modifier
+                    .padding(
+                        top = 4.dp,
+                        start = 16.dp
+                    )
+            )
+        }
+    }
 }
 
 @Preview(
@@ -48,6 +70,12 @@ fun ACTextFieldPreview() {
             Column(verticalArrangement = Arrangement.spacedBy(15.dp)) {
                 ACTextField(text = "Text Field Filled", onTextChanged = {}, labelText = "Label")
                 ACTextField(text = "", onTextChanged = {}, labelText = "Enter a text...")
+                ACTextField(
+                    text = "",
+                    onTextChanged = {},
+                    labelText = "Enter a text...",
+                    errorMessage = "This field is required"
+                )
             }
         }
     }
