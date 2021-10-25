8bd576e143ae99696a1faa4c5dc883ba75a84645
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2021-10-25 10:41:21 -0300

    Adding Edge to Edge UI experience (#43)

--- app/src/main/java/com/roblesdotdev/act/MainActivity.kt
+++ app/src/main/java/com/roblesdotdev/act/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.roblesdotdev.act.core.ui.theme.ACTasksTheme
 import com.roblesdotdev.act.login.ui.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,13 +18,28 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            ACTasksTheme {
-                LoginScreen(
-                    onLoginCompleted = {
-                        Log.d("ACTasksMainActivity", "Login has been completed")
-                    }
+            val systemUIController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+
+            SideEffect {
+                systemUIController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons,
                 )
+            }
+
+            ACTasksTheme {
+                ProvideWindowInsets {
+                    LoginScreen(
+                        onLoginCompleted = {
+                            Log.d("ACTasksMainActivity", "Login has been completed")
+                        }
+                    )
+                }
             }
         }
     }
